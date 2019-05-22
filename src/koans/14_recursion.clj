1fbdbf32c48f88c1c11ddce839b84eb536fcd381
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-05-22 11:45:56 +0200

    Finished

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,52 +3,63 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [[head & rest] coll
+         reversed []]
+    (if (nil? head)
+      reversed
+      (recur rest (into [] (cons head reversed))))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= 1 n)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
-  "Recursion ends with a base case"
-  (= true (is-even? 0))
+ "Recursion ends with a base case"
+ (= true (is-even? 0))
 
-  "And starts by moving toward that base case"
-  (= false (is-even? 1))
+ "And starts by moving toward that base case"
+ (= false (is-even? 1))
 
-  "Having too many stack frames requires explicit tail calls with recur"
-  (= false (is-even-bigint? 100003N))
+ "Having too many stack frames requires explicit tail calls with recur (extra)"
+ (= true (is-even-bigint? 100002N))
 
-  "Reversing directions is easy when you have not gone far"
-  (= '(1) (recursive-reverse [1]))
+ "Having too many stack frames requires explicit tail calls with recur"
+ (= false (is-even-bigint? 100003N))
 
-  "Yet it becomes more difficult the more steps you take"
-  (= '(6 5 4 3 2) (recursive-reverse [2 3 4 5 6]))
+ "Reversing directions is easy when you have not gone far"
+ (= '(1) (recursive-reverse [1]))
 
-  "Simple things may appear simple."
-  (= 1 (factorial 1))
+ "Yet it becomes more difficult the more steps you take"
+ (= '(6 5 4 3 2) (recursive-reverse [2 3 4 5 6]))
 
-  "They may require other simple steps."
-  (= 2 (factorial 2))
+ "Simple things may appear simple."
+ (= 1 (factorial 1))
 
-  "Sometimes a slightly bigger step is necessary"
-  (= 6 (factorial 3))
+ "They may require other simple steps."
+ (= 2 (factorial 2))
 
-  "And eventually you must think harder"
-  (= 24 (factorial 4))
+ "Sometimes a slightly bigger step is necessary"
+ (= 6 (factorial 3))
 
-  "You can even deal with very large numbers"
-  (< 1000000000000000000000000N (factorial 1000N))
+ "And eventually you must think harder"
+ (= 24 (factorial 4))
 
-  "But what happens when the machine limits you?"
-  (< 1000000000000000000000000N (factorial 100003N)))
+ "You can even deal with very large numbers"
+ (< 1000000000000000000000000N (factorial 1000N))
+
+ "But what happens when the machine limits you?"
+ (< 1000000000000000000000000N (factorial 100003N)))
